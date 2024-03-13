b574b973b07a3f10f6a06cf76930706ef9fd6d6e
Anbraten <6918444+anbraten@users.noreply.github.com>
2024-03-13 10:27:29 +0100

    fix yaml paths and cleanup docs

--- ansible.sh
+++ ansible.sh
@@ -4,6 +4,6 @@ RUN_ANSIBLE="docker run --rm -ti -v $(pwd):/ansible ansible"
 
 # docker build -t ansible .
 P=$@
-CMD="${P:=ansible-playbook site.yml}"
+CMD="${P:=ansible-playbook site.yaml}"
 
 $RUN_ANSIBLE $CMD

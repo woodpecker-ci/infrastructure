98db02cfed18c22d49747b2e6a3c21be67fa8aa2
Robert Kaussow <mail@thegeeklab.de>
2025-08-15 12:39:52 +0200

    Fix ci and cleanup (#160)

--- ssh.sh
+++ ssh.sh
@@ -5,7 +5,7 @@ SSH_KEY="keys/ci_woodpecker-ci_org.id_ed25519"
 if [ ! -f "$SSH_KEY" ]; then
   echo "🔑 Decrypting SSH keys ..."
   ./ansible.sh ansible-playbook playbooks/run.yaml --skip-tags always --tags ssh-key
-  ./ansible.sh chown --from=0:0 $(id -u):$(id -g) "$SSH_KEY"
+  ./ansible.sh chown --from=0:0 "$(id -u)":"$(id -g)" "$SSH_KEY"
 fi
 
 echo "🔑 Connecting to the server ..."
